9e1c3d06ce88ed48fd2508d3c216e664b264f285
GoldLeader2012 <isaacholland16@hotmail.co.uk>
2023-03-19 14:54:10 +0000

    Created a test for the read_csv function, then built the function and then called it in script 1.

--- python_survey_app/csvhelper/modules.py
+++ python_survey_app/csvhelper/modules.py
@@ -1,1 +1,11 @@
 # This file contains all the functions for the app to use in scipt1 and script2
+
+
+def read_csv(file_path):
+    data = []
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.strip() != "":
+                data.append(line.strip().split(","))
+    return data
