c1586af7958ed8801c28d41e522cb86a33542312
GoldLeader2012 <isaacholland16@hotmail.co.uk>
2023-03-19 15:04:01 +0000

    remove_duplicates test

--- python_survey_app/script1.py
+++ python_survey_app/script1.py
@@ -1,9 +1,12 @@
 # The input script is script1.py which reads the survey data from a file named results.csv located in the same directory.
 
-from python_survey_app.csvhelper.modules import read_csv
+from python_survey_app.csvhelper.modules import read_csv, remove_duplicates
 
 # Set the input file path
 input_file = "python_survey_app\\results.csv"
 
 # Read in the data from the input file
 data = read_csv(input_file)
+
+# Remove duplicates from the data
+unique_data = remove_duplicates(data)
--- python_survey_app/tests.py
+++ python_survey_app/tests.py
@@ -1,6 +1,6 @@
 # The Pytests for the functions defined in the modules.py file can be found in the tests.py file.
 
-from csvhelper.modules import read_csv
+from csvhelper.modules import read_csv, remove_duplicates
 
 
 def test_read_csv_file():
@@ -10,3 +10,14 @@ def test_read_csv_file():
     actual = read_csv("results.csv")
     # Check that the actual number of rows matches the expected number of rows
     assert len(actual) == expected
+
+
+def test_remove_duplicates():
+    # Read the contents of a CSV file using the read_csv function
+    data = read_csv("clean_results.csv")
+    # Call the remove_duplicates function to remove duplicate rows from the data
+    unique_data = remove_duplicates(data)
+    # Create a set of unique IDs from the unique_data list to check that all rows are unique
+    unique = set(row[0] for row in unique_data)
+    # Check that the number of unique rows in the unique_data list matches the number of unique IDs
+    assert len(unique_data) == len(unique)
