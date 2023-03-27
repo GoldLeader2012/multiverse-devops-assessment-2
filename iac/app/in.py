c1b7961a309f13602b9f320deb774b6ccc95da00
GoldLeader2012 <isaacholland16@hotmail.co.uk>
2023-03-27 16:56:23 +0100

    Redesign

---
+++ iac/app/in.py
@@ -0,0 +1,13 @@
+from app.csvhelper.modules import read_csv, remove_duplicates, clean_data
+
+# Set the input file path
+input_file = "root/code/iac/results.csv"
+
+# Read in the data from the input file
+data = read_csv(input_file)
+
+# Remove duplicates from the data
+unique_data = remove_duplicates(data)
+
+# Clean the data by capitalizing names and filtering answer 3 to be between 1 and 10
+cleaned_data = clean_data(unique_data)
