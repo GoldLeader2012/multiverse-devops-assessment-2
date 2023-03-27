60dfa72ee8829bce8dd026ec116ad47eda54544a
GoldLeader2012 <isaacholland16@hotmail.co.uk>
2023-03-27 20:50:45 +0100

    closerrr

--- python_survey_app/script2.py
+++
@@ -1,7 +0,0 @@
-from csvhelper.modules import write_csv
-
-# Set the output file path
-output_file = "clean_results.csv"
-
-# Write the cleaned data to the output file
-write_csv(output_file, cleaned_data)
